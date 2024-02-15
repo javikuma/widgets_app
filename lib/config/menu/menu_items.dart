f035bee46bf6eb320bf77a43c47a65682be17420
Javier Acosta <javieracosta2888@gmail.com>
2024-02-15 20:50:29 -0300

    animated container terminado

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -37,4 +37,10 @@ const appMenuItems = <MenuItem>[
       subtitle: 'Indicadores en pantalla',
       link: '/snackbars',
       icon: Icons.info_outline),
+  
+  MenuItem(
+      title: 'Animated Container',
+      subtitle: 'Stateful widget animado',
+      link: '/animated',
+      icon: Icons.check_box_outline_blank_rounded),
 ];
